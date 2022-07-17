96fee535b543f394afb721f22284658f48b49c8e
palaganaskurl <palaganaskurl@gmail.com>
2022-07-17 20:48:55 +0800

    Testing
    Tested some scenarios.

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import json
 from arena import Arena
 from item import Item
 from knight import Knight
+from knight import KnightStatus
 
 
 class Game:
@@ -11,24 +12,75 @@ class Game:
             Knight(color='Red', current_pos_x=0, current_pos_y=0),
             Knight(color='Blue', current_pos_x=7, current_pos_y=0),
             Knight(color='Green', current_pos_x=7, current_pos_y=7),
-            Knight(color='Yellow', current_pos_x=0, current_pos_y=7)
+            Knight(color='Yellow', current_pos_x=0, current_pos_y=7),
         ]
         initial_items = [
-            Item(name='Axe', attack=2, defense=0, current_pos_x=2, current_pos_y=2),
-            Item(name='Dagger', attack=1, defense=0, current_pos_x=2, current_pos_y=5),
-            Item(name='Helmet', attack=0, defense=1, current_pos_x=5, current_pos_y=5),
-            Item(name='MagicStaff', attack=1, defense=1, current_pos_x=5, current_pos_y=2),
+            Item(
+                name='Axe',
+                attack=2,
+                defense=0,
+                current_pos_x=2,
+                current_pos_y=2,
+            ),
+            Item(
+                name='Dagger',
+                attack=1,
+                defense=0,
+                current_pos_x=2,
+                current_pos_y=5,
+            ),
+            Item(
+                name='Helmet',
+                attack=0,
+                defense=1,
+                current_pos_x=5,
+                current_pos_y=5,
+            ),
+            Item(
+                name='MagicStaff',
+                attack=1,
+                defense=1,
+                current_pos_x=5,
+                current_pos_y=2,
+            ),
         ]
+        # # Tester for multiple initial items on same x and y coordinates.
+        # initial_items = [
+        #     Item(
+        #         name='Axe',
+        #         attack=2,
+        #         defense=0,
+        #         current_pos_x=5,
+        #         current_pos_y=5,
+        #     ),
+        #     Item(
+        #         name='Dagger',
+        #         attack=1,
+        #         defense=0,
+        #         current_pos_x=5,
+        #         current_pos_y=5,
+        #     ),
+        #     Item(
+        #         name='Helmet',
+        #         attack=0,
+        #         defense=1,
+        #         current_pos_x=5,
+        #         current_pos_y=5,
+        #     ),
+        #     Item(
+        #         name='MagicStaff',
+        #         attack=1,
+        #         defense=1,
+        #         current_pos_x=5,
+        #         current_pos_y=5,
+        #     ),
+        # ]
         self.arena = Arena(initial_knights, initial_items)
 
-        print(self.arena)
-
     def run(self):
-        with open('moves2.txt', 'r') as f:
+        with open('all_drowning_moves.txt', 'r') as f:
             moves = f.read().splitlines()
             moves = moves[1:-1]
-
-            print(moves)
 
             for move in moves:
                 parsed_move = move.split(':')
@@ -41,18 +93,23 @@ class Game:
         json_data = {}
 
         for knight in self.arena.knights.values():
+            if knight.status == KnightStatus.DROWNED:
+                position = None
+            else:
+                position = [knight.current_pos_x, knight.current_pos_y]
+
             json_data[knight.color] = [
-                [knight.current_pos_x, knight.current_pos_y],
+                position,
                 knight.status,
                 knight.item.name if knight.item else None,
                 knight.total_attack,
-                knight.total_defense
+                knight.total_defense,
             ]
 
         for item in self.arena.items.values():
             json_data[item.name] = [
                 [item.current_pos_x, item.current_pos_y],
-                item.equipped
+                item.equipped,
             ]
 
         with open('final_state.json', 'w') as f:
@@ -61,10 +118,13 @@ class Game:
 
 if __name__ == '__main__':
     game = Game()
-    game.run()
+
+    print('Initial Arena')
     print(game.arena)
 
-    from pprint import pprint
-    pprint(game.arena.knights)
+    game.run()
+
+    print('Arena After Running Game')
+    print(game.arena)
 
     game.export()
